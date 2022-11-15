5f61c114143b49519be18f76d5da5649f1d98e3c
ViliKo <59921526+ViliKo@users.noreply.github.com>
2022-11-15 12:28:09 +0200

    wordpress theme start

---
+++ functions.php
@@ -0,0 +1,7 @@
+
+<?php
+function bluespark_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/assets/style.css');
+}
+add_action( 'wp_enqueue_scripts', 'bluespark_styles' );
